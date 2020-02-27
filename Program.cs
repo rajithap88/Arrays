4024c4a6070186b7918b26e6f04401689f09992d
rajithap88 <56268090+rajithap88@users.noreply.github.com>
2020-02-27 13:46:33 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Linq;
+
+namespace Asg2_q2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] l2 = new int[] { 0, 2, 2, 2, 7,7,7,17,50 };
+
+            for (int i = 0; i < (l2.Length-1); i++)
+            {
+
+
+                if (l2[i] > l2[i + 1])
+                {
+                    l2[i + 1] = l2[i];
+                }
+                if (l2[i] == l2[i + 1])
+                    {
+                        
+                        l2[i+1] = l2[i] + 1;
+                    }
+                
+               
+
+            }
+           
+            int sum1 = l2.Sum();
+            for (int j = 0; j < l2.Length; j++)
+            {
+                //Console.WriteLine(sum);
+                Console.WriteLine(l2[j]);
+                // int final = (sum + l2[j]);
+               // sum += l2[j];
+
+                
+            }
+            Console.Write(sum1);
+         
+        }
+    }
+}
